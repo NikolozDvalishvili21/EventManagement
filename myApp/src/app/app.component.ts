ebffab72c54bf3a8cd9c3e5e5ebd60d1b2a2138f
Nika <nikadvalishvili517@gmail.com>
2025-01-12 22:35:51 +0400

    made header

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomepageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'myApp';
