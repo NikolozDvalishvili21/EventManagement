61a75af39adb3717c95eaa6da150953f8be4c217
Nika <nikadvalishvili517@gmail.com>
2025-01-12 22:58:31 +0400

    made popular music

---
+++ myApp/src/app/components/popular-sect/popular-sect.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-sect',
+  imports: [],
+  templateUrl: './popular-sect.component.html',
+  styleUrl: './popular-sect.component.scss'
+})
+export class PopularSectComponent {
+
+}
--- myApp/src/app/pages/homepage/homepage.component.ts
+++ myApp/src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { PopularSectComponent } from '../../components/popular-sect/popular-sect.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PopularSectComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
