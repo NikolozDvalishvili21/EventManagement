d3252e1ad8e192dbbb5ffdf5dbbd4cacbdab7705
Nika <nikadvalishvili517@gmail.com>
2025-01-16 19:50:22 +0400

    made footer

--- myApp/src/app/pages/homepage/homepage.component.ts
+++ myApp/src/app/pages/homepage/homepage.component.ts
@@ -4,9 +4,10 @@ import { PopularSectComponent } from '../../components/popular-sect/popular-sect
 import { EventsComponent } from '../../components/events/events.component';
 import { FreeEventsComponent } from '../../components/free-events/free-events.component';
 import { UsersComponent } from '../../components/users/users.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-homepage',
-  imports: [HeaderComponent, PopularSectComponent, EventsComponent, FreeEventsComponent, UsersComponent],
+  imports: [HeaderComponent, PopularSectComponent, EventsComponent, FreeEventsComponent, UsersComponent, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
